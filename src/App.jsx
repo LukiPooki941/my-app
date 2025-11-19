58bbc6d7ae2ad262d37f8f75c5c49fdc550a1c89
Luke Cherniawski <lukecherniawski@Lukes-MacBook-Pro.local>
2025-11-18 23:07:19 -0700

    I made some new files and made some static nfunctions. Will work on ordering those functions tommorrow

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SearchBar from './SearchBar'
+import SearchResults from './SearchResults'
+import PlayList from './PlayList'
+import Track from './Track'
+import TrackList from './TrackList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+return(
+  <>
+  <SearchBar />
+  <SearchResults />
+  <TrackList />
+  <PlayList />
+  <p>Now I am in control</p>
+  <button>Save To Spotify</button>
+  <button>Search</button>
+  </>
+)
 }
 
 export default App
---
+++ src/TrackList2.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+function TrackList_2(){
+
+}
+
+export default TrackList_2
