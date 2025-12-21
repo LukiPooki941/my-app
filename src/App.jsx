63b4ead7d6145880d2ea021455613f15022134b7
Luke Cherniawski <lukecherniawski@Lukes-MacBook-Pro.local>
2025-12-20 18:00:22 -0700

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import SearchBar from './SearchBar'
 import SearchResults from './SearchResults'
 import PlayList from './PlayList'
-import { Authenticater } from './userauth'
-//import { api_key } from './userauth'
+import queryString from 'query-string';
+
+
 const defaultName = "My PlayList";
+let api_key = '';
 const my_test_array = [
 {
   name: 'sound of silence',    
@@ -27,11 +29,29 @@ const my_test_array = [
 function App() {
   const[song, setSong] = useState([])
   const[playlist, setPlaylist] = useState(defaultName)
+  const[number, setNumber] = useState(1);
+  
 
   const client_id = '5512ce31c5264f6eaa0f445c2c9fd0c1';
   const redirect_uri = 'http://127.0.0.1:5174/callback';
   const client_secret = '6ed25e1051ed4122a2113ad74c57652d';
   const baseURL = 'https://accounts.spotify.com/api/token';
+  const scope = 'user-read-private user-read-email';
+  const url = 'https://accounts.spotify.com/authorize?' +
+    queryString.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri
+    })
+
+
+function Authenticater() {
+   window.location.href = url;
+
+
+}
+
   
   const getAuthorization = async () => {
         try{
@@ -59,7 +79,8 @@ function App() {
             if(response.ok){
                 const jsonResponse = await response.json()
                 console.log(jsonResponse);
-
+                api_key = jsonResponse;
+               
 
     
             }
@@ -68,7 +89,7 @@ function App() {
         }
       }
 
-      getAuthorization();
+      getAuthorization()
 
   const handleSubmit = (e) => {
     for(let element in song)
@@ -80,12 +101,17 @@ function App() {
      setSong([]);
      setPlaylist(defaultName);
   }
-  
-  
+
+
+if(number == 1){
+  return(<>
+    <h1>Welcome to Jammming</h1>
+    <button onClick={() => {Authenticater();}}>press to begin</button>
+  </>)
+} else { 
 return(
   <>
   <div>
-  <button onClick={Authenticater}>press me</button>
   <SearchBar />
   <button type='submit'>Search</button>
   <SearchResults setSong={setSong} object={my_test_array} />
@@ -96,5 +122,5 @@ return(
 
 )
 }
-
+}
 export default App
